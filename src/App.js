5c334dedeb33f54a688d1c0c0155c312d9d9f32b
AGS Lakpahana <agslakpahana@gmail.com>
2023-09-01 20:10:19 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Container,Box, Grid, Typography, TextField, Toolbar, Button } from "@mui/material";
+import CalculateIcon from '@mui/icons-material/Calculate';
+import { useState } from "react";
 function App() {
+
+  //states for input values
+  const [gain, setGain] = useState(null);
+  const [bandwidth, setBandwidth] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {/* simple toolbar with App Name */}
+      <Toolbar
+
+      >
+        <CalculateIcon sx={{ mr: 2 }} />
+        <Typography variant="h6" color="inherit" component="div">
+          PCB Calculator
+        </Typography>
+
+        <Button
+          onClick={() => {
+            window.location.href = "https://github.com/lakpahana"
+          }
+          }
+          variant="outlined" sx={{
+            marginLeft: "auto"
+
+
+
+          }}>contribute</Button>
+      </Toolbar>
+      <Grid container spacing={2} >
+        <Grid item xs={4}>
+
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              textAlign: "center"
+            }}
+          >Input Details</Typography>
+
+
+          <Box
+          sx={{
+            display:"flex",
+            flexDirection:"column",
+            gap:2
+          
+          }}
+          >
+            <TextField 
+            
+            onChange={(e)=>{
+              setGain(e.target.value)
+            }
+            }
+            id="outlined-basic"
+            value={gain}
+            label="Gain" variant="outlined" 
+            type="number"/>
+            <TextField
+             onChange={(e)=>{
+              setBandwidth(e.target.value)
+             }}
+            id="outlined-basic" label="BandWidth" 
+            type="number"
+            value={bandwidth} variant="outlined" />
+          </Box>
+
+
+
+
+
+
+
+
+
+
+        </Grid>
+        <Grid item xs={8}>
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              textAlign: "center"
+            }}
+          >Output Details</Typography>	
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
